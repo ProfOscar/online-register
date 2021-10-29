17f45d9618d759c27d813fb0c43699bd41453fb4
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2021-10-29 13:12:14 +0200

    WebForm project started

---
+++ OnlineRegisterWebForm/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="OnlineRegisterWebForm.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="BtnStudent" runat="server" Text="STUDENTI" OnClick="BtnStudent_Click" />
+            <asp:Button ID="BtnTeacher" runat="server" Text="DOCENTI" OnClick="BtnTeacher_Click" />
+            <br />
+            <asp:Label ID="LblOutput" runat="server" Text="(qui appariranno i dati)"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ OnlineRegisterWebForm/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineRegisterWebForm
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnStudent_Click(object sender, EventArgs e)
+        {
+            // OnlineRegisterClassLibrary.DbTools db;
+            LblOutput.Text = GetTableList("Student");
+        }
+
+        public string GetTableList(string tableName)
+        {
+            const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\data\onlineregister\register.mdf;Integrated Security=True;Connect Timeout=30";
+
+            string stOut = "";
+            using (SqlConnection dbConn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand dbCmd = new SqlCommand("SELECT * FROM " + tableName, dbConn))
+                {
+                    dbConn.Open();
+                    using (SqlDataReader dbReader = dbCmd.ExecuteReader())
+                    {
+                        while (dbReader.Read())
+                        {
+                            for (int i = 0; i < dbReader.FieldCount; i++)
+                            {
+                                stOut += dbReader.GetValue(i) + " - ";
+                            }
+                            stOut = stOut.Remove(stOut.Length - 3) + "\n";
+                        }
+                    }
+                }
+            }
+            return stOut;
+        }
+
+        protected void BtnTeacher_Click(object sender, EventArgs e)
+        {
+            LblOutput.Text = GetTableList("Teacher");
+        }
+    }
+}
